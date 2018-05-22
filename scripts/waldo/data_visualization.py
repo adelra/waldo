1bb5db86dc4827b53317decdaa81616e0ec8bde2
Adel <rahimi.adel@gmail.com>
2018-05-22 18:10:13 +0430

    editted conflicts

--- scripts/waldo/data_visualization.py
+++ scripts/waldo/data_visualization.py
@@ -9,39 +9,10 @@ import numpy as np
 from PIL import Image
 from io import BytesIO
 
-<<<<<<< HEAD
-""" TODO
-"""
-"""randomly coloring white areas. low is 10 as a threshold.
- dimensions are = Red, Blue, Green, Alpha
- Alpha is set 38 for a ~20% transparency"""
-def visualize_mask(x):
-    validate_image_with_mask(x)
-    mask = x['mask']
-    red, green, blue, alpha = mask.T
-    white_areas = (red > 0) & (blue > 0) & (green > 0)
-    black_areas = (red == 0) & (blue == 0) & (green == 0) & (alpha == 255)
-=======
 from waldo.data_types import *
->>>>>>> b3ce8db107dc626e908ba139c46f5afb9aae3774
 
-    mask[..., :][white_areas.T] = (
-        np.random.randint(low=10, high=255), np.random.randint(low=10, high=255),
-        np.random.randint(low=10, high=255), 38)
-    mask[..., :][black_areas.T] = 0
 
-    x['mask'] = mask
-    validate_image_with_mask(x)
-    return None
-
-<<<<<<< HEAD
-    """This function accepts an object x that should represent an image with a
-       mask, and it modifies the image to superimpose the "mask" on it.  The
-       image will still be visible through a semi-transparent mask layer.
-       This function returns None; it modifies x in-place.
-=======
 def visualize_mask(x, c, transparency=0.3):
->>>>>>> b3ce8db107dc626e908ba139c46f5afb9aae3774
     """
     This function accepts an object x that should represent an image with a
     mask, a config class c, and a float 0 < transparency < 1.  
